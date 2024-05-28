703c180cee9ea63c490a4b45d5ce035b9cea8536
Elmin N. Mammadov <elmin.mammadov2@kapitalbank.az>
2024-05-28 14:52:41 +0400

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <Button className="" variant="ghost">Click me</Button>
+    </>
+  )
+}
+
+export default App
