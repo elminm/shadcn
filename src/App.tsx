70defe2cf299a466ef049d5c1164209e31ca7a05
Elmin N. Mammadov <elmin.mammadov2@kapitalbank.az>
2024-05-28 15:08:01 +0400

    date picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button"
+import { DatePicker } from "./components/ui/date-picker"
 
 function App() {
 
   return (
     <>
       <Button className="" variant="ghost">Click me</Button>
+      <DatePicker/>
     </>
   )
 }
